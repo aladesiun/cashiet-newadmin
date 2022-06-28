8e9daa6e296b2ad95b6818a6ab23363e376a7662
temitope <aladesiuntope@gmail.com>
2022-06-28 09:28:03 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/dist/css/app.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="" element={<App />}>
+        <Route path='/' element={<Home/>}></Route>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
