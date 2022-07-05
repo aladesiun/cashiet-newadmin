d1ae56546fe93e18d9a1d9ecf4f7e4dad2eb29b8
temitope <aladesiuntope@gmail.com>
2022-07-05 11:05:34 +0100

    update

--- src/views/products/product-list.js
+++ src/views/products/product-list.js
@@ -6,20 +6,19 @@ import httpServices from "../../hooks/http-services";
 import { Link } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import Product from "./product";
 
 const Products = () => {
     let navigate = useNavigate()
     const [Loading, setLoading] = useState(false);
     const [Products, setProducts] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams]= useSearchParams();
     const [query, setQuery] = useState(searchParams.get('name'))
     const url = query ? `/products/filter/${query}` : "/products/filter";
     const handleInputChange = (e) => {
         const newQuery = e.target.value;
         setQuery(newQuery);
         setSearchParams({
-            name: newQuery
+            name:newQuery 
         })
 
     }
@@ -70,15 +69,49 @@ const Products = () => {
     useEffect(() => {
         getProducts()
     }, []);
-
+   
     return (
         <div className="page-body ">
             <div className="product-cont">
                 <div className="col-xl-12 xl-100">
                     <div className="card height-equal">
+                        
+                        <div className="card-header">
+                            <h5>Products</h5>
+                            <div className="card-header-right">
+                                <ul className="list-unstyled card-option">
+                                    <li><i className="icofont icofont-simple-left"></i></li>
+                                    <li><i className="view-html fa fa-code"></i></li>
+                                    <li><i className="icofont icofont-maximize full-card"></i></li>
+                                    <li><i className="icofont icofont-minus minimize-card"></i></li>
+                                    <li><i className="icofont icofont-refresh reload-card"></i></li>
+                                    <li><i className="icofont icofont-error close-card"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-between align-items-center bg-white p-3">
+                            <form className="form-inline search-form search-box" onSubmit={(e)=>{e.preventDefault(); getProducts()}}>
+                                <div className="form-group flex">
+                                    <input className="form-control" type="text" placeholder="Search.." value={query} onChange={handleInputChange}/>
+
+                                </div>
+                            </form>
+                            <Link to="page-create.html" className="btn btn-primary mt-md-0 mt-2">Add New Page</Link>
+                        </div>
                         <div className="card-body">
                             <div className="user-status table-responsive products-table">
                                 <table className="table table-bordernone mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Image</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Slug</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">Keywords</th>
+                                            <th scope="col"></th>
+                                            <th scope="col"></th>
+                                        </tr>
+                                    </thead>
                                     <tbody>
                                         {Loading &&
                                             <tr>
@@ -104,95 +137,33 @@ const Products = () => {
                                             </tr>
                                         }
 
-                                        
+                                        {Products && Products.map((product) => (<tr key={product._id} >
+                                            <td className="img-td" ><img src={product.image.url} alt={product._id + "product"} className="" style={{ width: '118px', height:"77px", borderRadius:'10px' }}></img></td>
+
+                                            <td>{product.name}</td>
+                                            <td>{product.slug}</td>
+                                            <td>{product.price}</td>
+                                            <td>{product.keywords}</td>
+                                            <td>{product._id}</td>
+                                            <td>
+                                                <div className="form-button">
+                                                    <Link className="btn btn-primary" to={"/product/" + product._id}>View</Link>
+                                                    <div className="form-button m-4 flex" >
+                                                        <Link className="" to={"/product/edit/" + product._id}>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></Link>
+                                                        <Link to="#" className="m-2" onClick={() => { deleteProduct(product._id) }}>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                                                        </Link>
+                                                    </div>
+                                                </div>
+
+                                            </td>
+
+                                        </tr>
+                                        ))}
 
                                     </tbody>
                                 </table>
-                            </div>
-                            <div className="page-body">
-                                {/* Container-fluid starts*/}
-                                <div className="container-fluid">
-                                    <div className="page-header">
-                                        <div className="row">
-                                            <div className="col-lg-6">
-                                                <div className="page-header-left">
-                                                    <h3>
-                                                        Product List
-                                                        <small>Cashiet Admin panel</small>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <ol className="breadcrumb pull-right">
-                                                    <li className="breadcrumb-item">
-                                                        <a href="index.html">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
-                                                                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                                                                <polyline points="9 22 9 12 15 12 15 22" />
-                                                            </svg>
-                                                        </a>
-                                                    </li>
-                                                    <li className="breadcrumb-item">Physical</li>
-                                                    <li className="breadcrumb-item active">Product List</li>
-                                                </ol>
-                                            </div>
-                                        </div>
-                                        <div className="d-flex justify-content-between align-items-center bg-white p-3">
-                                            <form className="form-inline search-form search-box" onSubmit={(e) => { e.preventDefault(); getProducts() }}>
-                                                <div className="form-group flex">
-                                                    <input className="form-control" type="text" placeholder="Search.." value={query} onChange={handleInputChange} />
-
-                                                </div>
-                                            </form>
-                                            <Link to="page-create.html" className="btn btn-primary mt-md-0 mt-2">Add New Page</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                                {/* Container-fluid Ends*/}
-                                {/* Container-fluid starts*/}
-                                <div className="container-fluid">
-                                    <div className="row products-admin ratio_asos">
-                                    {Products && Products.map((product) => (
-                                              <div className="col-xl-3 col-sm-6" key={product.id}>
-                                              <div className="card">
-                                                  <div className="card-body product-box">
-                                                      <div className="img-wrapper">
-                                                          <div className="front">
-                                                              <a href="javascript:void(0)" className="bg-size " style={{"background-size": "cover", "background-position": "center center", "display": "block" }}>
-                                                                  {/* <img src={require(Products &  product.image.url)} className="img-fluid blur-up lazyload bg-img" alt style={{ "display": "" }} /> */}
-                                                              </a>
-                                                              <div className="product-hover">
-                                                                  <ul>
-                                                                      <li>
-                                                                          <button className="btn" type="button" data-original-title title><i className="ti-pencil-alt" /></button>
-                                                                      </li>
-                                                                      <li>
-                                                                          <button className="btn"><i className="ti-trash" /></button>
-                                                                      </li>
-                                                                  </ul>
-                                                              </div>
-                                                          </div>
-                                                      </div>
-                                                      <div className="product-detail">
-                                                          <div className="rating"><i className="fa fa-star" /> <i className="fa fa-star" /> <i className="fa fa-star" /> <i className="fa fa-star" /> <i className="fa fa-star" /></div>
-                                                          <a href="javascript:void(0)">
-                                                              <h6>{product.name}</h6>
-                                                          </a>
-                                                          <h4>{product.price}<del>$600.00</del></h4>
-                                                          <ul className="color-variant">
-                                                              <li className="bg-light0" />
-                                                              <li className="bg-light1" />
-                                                              <li className="bg-light2" />
-                                                          </ul>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                            ))}
-                                      
-                                    </div>
-                                </div>
-                                {/* Container-fluid Ends*/}
                             </div>
                             <div className="code-box-copy">
                                 <button className="code-box-copy__btn btn-clipboard"
