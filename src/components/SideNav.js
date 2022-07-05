0d82d92e022f95a6f824762836825f2288788105
temitope <aladesiuntope@gmail.com>
2022-07-05 08:41:07 +0100

    update

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -29,8 +29,8 @@ const SideNavHeader = () => {
                     </div>
                 </div>
                 <ul className="sidebar-menu">
-                    <li className="active">
-                        <a className="sidebar-header active" href="/">
+                    <li className="">
+                        <a className="sidebar-header" href="/">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                             <span>Dashboard</span>
                         </a>
